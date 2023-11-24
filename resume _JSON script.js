94c8dee572710235fd30cd55a7c75f1cc3434a1e
nandhini perumal_FSD_developer <150987579+nandhini1227@users.noreply.github.com>
2023-11-24 13:48:27 +0530

    resume _JSON script.js

---
+++ resume _JSON script.js
@@ -0,0 +1,58 @@
+let myResume={
+    "basics": {
+      "name": "Nandhini Perumal",
+      "email": "nandhuvigneshpb13@gmail.com",
+      "phone": 8883020083,
+      "degree": "M.E",
+      "location": {
+        "address": "57G Kamaraja puram north street ,Arumugam Road, Tirumangalam ",
+        "pincode": 625706,
+        "city": "madurai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/nandhu1294/",
+          "github":"https://github.com/nandhini1227"
+        }
+      ],
+"summary": [
+         "Strong expertise in Python, JavaScript, and related technologies, including Node.js, React, AWS, and Git.","Excellent problem-solving and analytical skills, with a proven track record of successfully completing complex tasks.","Experienced in collaborating with cross-functional teams and delivering high-quality software products.",
+         ] "education": [
+      {
+        "institution": "velammal  College Of Engineering and Technology",
+        "department": "Computer Science and Engineering",
+        "studyType": "fulltime",
+        "batch start year": 2017,
+        "batch end year": 2019,
+        "gpa": 8.5,
+      }
+    ],
+    "skills": [
+      {  
+         "Python",
+        "JavaScript",
+        "Node.js",
+        "React",
+        "AWS",
+        "Git"        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "Hobbies": [
+      {
+       Reading.
+Learning a new language.
+Journaling.
+cooking.
+Gardening.  ,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
